433a04d249ab56c1a04124e3b8271db4aebe7848
Gates XII <andrew@xobin.com>
2017-04-01 10:28:49 +0530

    frontend

---
+++ js/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+angular.module('AngularFlask', ['angularFlaskServices'])
+	.config(['$routeProvider', '$locationProvider', '$httpProvider',
+		function($routeProvider, $locationProvider, $httpProvider) {
+
+		$routeProvider
+		.when('/', {
+			templateUrl: 'partials/landing.html',
+			controller: IndexController
+		})
+    .when('/hospitals', {
+			templateUrl: 'partials/hospitals.html',
+			controller: HospitalsController
+		})
+		.when('/gghos', {
+			templateUrl: 'partials/gghos.html',
+			controller: gghosController
+		})
+		.when('/road', {
+			templateUrl: 'partials/road.html',
+			controller: roadController
+		})
+		.when('/commodities', {
+			templateUrl: 'partials/allcomo.html',
+			controller: allcomoController
+		})
+		.otherwise({
+			redirectTo: '/'
+		})
+		;
+
+		$locationProvider.html5Mode(true);
+		$httpProvider.defaults.headers.common = {};
+	  $httpProvider.defaults.headers.post = {};
+	  $httpProvider.defaults.headers.put = {};
+	  $httpProvider.defaults.headers.patch = {};
+		$httpProvider.defaults.headers.get = {};
+	}])
+;
